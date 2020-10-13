793f99100135b76a120b92a5ddcac972a6621713
Emeline Barbeau <emeline.barbeau@polymtl.ca>
2020-10-13 16:26:18 -0400

    Fini + Commentaires fait

--- exercice.py
+++ exercice.py
@@ -10,32 +10,60 @@ def reverse_data(data: list = None):
     # les stocker dans une structure de données,
     # et les retourner en ordre inverse, sans utiliser de liste.
 
-    if data is None:
-        pass  # Demander les valeurs ici
+    if data is None : # Demander les valeurs ici
+        data = [input("Entrez une valeur : ") for _ in range(10)]
 
-    reversed_data = None  # Stocker le résultat ici
+    reversed_data = Stack()
+    reversed_data.put_many(data)  # Stocker le résultat ici
 
-    return reversed_data
+    return [reversed_data.get() for _ in range(10)]  # Poper les valeurs pour les avoir dans le sens inverse
 
 
 def delete_nth_from_stack(data: Stack, position: int) -> Stack:
     # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
-    return Stack()
+
+    temp = [data.get() for _ in range(len(data) - position - 1)]  # -1 car une valeur ne doit pas être mise dans la liste temporaire
+    data.get()
+
+    temp.reverse() 
+    data.put_many(temp)
+
+    return data
 
 
 def delete_nth_from_queue(data: Queue, position: int) -> Queue:
     # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
-    return Queue()
+   
+    size_queue = len(data)
+
+    temp = [data.get() for _ in range(position)]
+    data.put_many(temp)
+
+    data.get()
+    
+    temp = [data.get() for _ in range(size_queue - position - 1)]  # -1 car une valeur ne doit pas être mise dans la liste temporaire
+    data.put_many(temp)
+
+    return data
 
 
 def sort_stack(data: Stack) -> Stack:
     # TODO: Retourner la séquence triée
-    return Stack()
+
+    temp = [data.get() for _ in range(len(data))]
+    temp.sort()
+    data.put_many(temp)
+
+    return data
 
 
 def sort_queue(data: Queue) -> Queue:
     # TODO: Retourner la séquence triée
-    return Queue()
+    temp = [data.get() for _ in range(len(data))]
+    temp.sort()
+    data.put_many(temp)
+
+    return data
 
 
 def string_and_structs(string: str) -> tuple:
@@ -44,6 +72,13 @@ def string_and_structs(string: str) -> tuple:
     # Sinon, on retire un élément de fifo pour l'insérer dans lifo.
 
     fifo, lifo = Queue(), Stack()
+
+    for char in string :
+        if ('a' <= char) and (char <= 'z') or ('A' <= char) and (char <= 'Z') :  # Minuscule ou majuscule
+            fifo.put(char)
+
+        else :
+            lifo.put(fifo.get())
 
     return fifo, lifo
 
@@ -64,7 +99,7 @@ def main() -> None:
 
     lifo = Stack()
     lifo.put_many([randint(0, 1000) for _ in range(20)])
-    print(f"On ordonne une file: {sort_queue(lifo)}")
+    print(f"On ordonne une file: {sort_stack(lifo)}")
 
     fifo = Queue()
     fifo.put_many([randint(0, 1000) for _ in range(20)])
